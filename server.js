6780c8dbd2e147ef691e676d0688e43e69e92794
sean.p.arnold@gmail.com <sean.p.arnold@gmail.com>
2021-08-17 12:36:24 -0700

    made changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -44,12 +44,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.sendStatus(404);
+    }
   });
 
 app.listen(PORT, () => {
